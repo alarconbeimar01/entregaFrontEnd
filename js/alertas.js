ea1f8bd5e34e785798ede5977f3dc16e1da65316
Beimar Alarcon <alarconbeimar@gmail.com>
2021-04-17 19:47:43 -0500

    Create alertas.js

---
+++ js/alertas.js
@@ -0,0 +1,25 @@
+function preguntarRecaudar() {
+	if(confirm("Desea recaudar la mensualidad ?")){
+		return true;
+	}
+	else{
+		return false;
+		}
+}
+
+
+
+function validarContrasenas(form)
+{
+    if(form.password.value == form.confirmarpassword.value)
+    { form.submit();
+	alert("Registro exitoso");
+	}
+    else
+    {
+		
+    alert("La contraseña no coincide.");
+    return false;
+    }
+}
+   
